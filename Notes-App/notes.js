e0cccb36c827e6a74ca1f1e3e4bc33b25b945813
manish-giri <manish.giri.me@gmail.com>
2018-02-07 23:43:49 -0500

    Refactor into helper functions

--- Notes-App/notes.js
+++ Notes-App/notes.js
@@ -1,5 +1,23 @@
 const fs = require('fs');
 
+
+const fetchNotes = () => {
+    // fetch existing notes from file system
+    try {
+        let currentNoteString = fs.readFileSync('notes-data.json');
+        return JSON.parse(currentNoteString);
+    }
+    catch (e) {
+        console.log("=== Reading from file failed ===");
+        return [];
+    }
+};
+
+const saveNotes = notes => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    console.log('New note successfully added!');
+}
+
 const addNote = (title, body) => {
     console.log(`Adding note => ${title}: ${body}`);
 
@@ -7,16 +25,7 @@ const addNote = (title, body) => {
     const note = {title, body};
 
     // prep array to store notes
-    let notes = [];
-
-    // fetch existing notes from file system
-    try {
-        let currentNoteString = fs.readFileSync('notes-data.json');
-        notes = JSON.parse(currentNoteString);
-    }
-    catch (e) {
-        console.log("=== Reading from file failed ===");
-    }
+    let notes = fetchNotes();
 
     // check if new note title is unique
     let duplicateNotes = notes.filter(note => note.title === title);
@@ -26,8 +35,7 @@ const addNote = (title, body) => {
     if(duplicateNotes.length === 0) {
         notes.push(note);
         // add to file system
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-        console.log('New note successfully added!');
+       saveNotes(notes);
     }
 
 
